e0c33521adb1785eacfac4d6ee4cc4ad524413c9
Boran Yildirim <boranyldrm50@gmail.com>
2020-12-05 20:10:44 -0500

    cluster

---
+++ infrastructure/cluster/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+	default = "csye7125-gcp-project"
+}
+
+variable "terraformcredentialspath" {
+	
+}
+
+variable "bucket" {
+	
+}
+
+variable "services" {
+  type = list(string)
+}
